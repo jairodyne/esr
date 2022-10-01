81a0b32f2bc8eb46c9b6f6b1cbd1f38a123c3754
Jairo <jairo.carvalho@conam.com.br>
2022-10-01 14:24:52 -0300

    20.11. Implementando o versionamento da API por Media Type

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.algaworks.algafood.api.model.input.ItemPedidoInput;
 import com.algaworks.algafood.api.v1.model.EnderecoModel;
+import com.algaworks.algafood.api.v1.model.input.ItemPedidoInput;
+import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.ItemPedido;
 
@@ -15,6 +17,10 @@ public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
+		
+		// Em razão da V2 ter idEstado, está sobrepondo o Cidade.id. Então, é pra pular isso.
+		modelMapper.createTypeMap(CidadeInputV2.class, Cidade.class)
+		.addMappings(mapper -> mapper.skip(Cidade::setId));
 		
 		// para não atribuir o id do produtoId para  item no pedido (que é gerado pela sequence do banco)
 		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
