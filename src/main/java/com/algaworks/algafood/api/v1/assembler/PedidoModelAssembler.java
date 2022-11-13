9b0f71448c0abfa9491124928267a1a3342211a7
jairo <jairo.carvalho@conam.com.br>
2022-11-12 21:57:11 -0300

    23.39. Gerando links do HAL dinamicamente de acordo com permissões do usuário

--- src/main/java/com/algaworks/algafood/api/v1/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/v1/assembler/PedidoModelAssembler.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.api.v1.AlgaLinks;
 import com.algaworks.algafood.api.v1.controller.PedidoController;
 import com.algaworks.algafood.api.v1.model.PedidoModel;
+import com.algaworks.algafood.core.security.AlgaSecurity;
 import com.algaworks.algafood.domain.model.Pedido;
 
 @Component
@@ -20,6 +21,9 @@ public class PedidoModelAssembler
     @Autowired
     private AlgaLinks algaLinks;
     
+    @Autowired
+    private AlgaSecurity algaSecurity;
+    
     public PedidoModelAssembler() {
         super(PedidoController.class, PedidoModel.class);
     }
@@ -30,17 +34,21 @@ public class PedidoModelAssembler
         modelMapper.map(pedido, pedidoModel);
         
         pedidoModel.add(algaLinks.linkToPedidos("pedidos"));
-        
-        if(pedido.podeSerConfirmado()) {
-        	pedidoModel.add(algaLinks.linkToConfirmacaoPedido(pedido.getCodigo(), "confirmar"));
-        }
-        
-        if(pedido.podeSerCancelado()) {
-        	pedidoModel.add(algaLinks.linkToCancelamentoPedido(pedido.getCodigo(), "cancelar"));
-        }
-
-        if(pedido.podeSerEntregue()) {
-        	pedidoModel.add(algaLinks.linkToEntregaPedido(pedido.getCodigo(), "entregar"));
+      
+        if(algaSecurity.podeGerenciarPedidos(pedido.getCodigo())) {
+        	
+        	if(pedido.podeSerConfirmado()) {
+        		pedidoModel.add(algaLinks.linkToConfirmacaoPedido(pedido.getCodigo(), "confirmar"));
+        	}
+        	
+        	if(pedido.podeSerCancelado()) {
+        		pedidoModel.add(algaLinks.linkToCancelamentoPedido(pedido.getCodigo(), "cancelar"));
+        	}
+        	
+        	if(pedido.podeSerEntregue()) {
+        		pedidoModel.add(algaLinks.linkToEntregaPedido(pedido.getCodigo(), "entregar"));
+        	}
+        	
         }
         
         pedidoModel.getRestaurante().add(
