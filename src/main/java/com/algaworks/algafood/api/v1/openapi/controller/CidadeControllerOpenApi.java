cb6cb11a7cef69368d0748d7a93abd7ad02dab65
jairo <jairo.carvalho@conam.com.br>
2023-02-17 22:12:30 -0300

    26.8. Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -7,6 +7,8 @@ import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -18,15 +20,16 @@ public interface CidadeControllerOpenApi {
 	CollectionModel<CidadeModel> listar();
 
 	@Operation(summary = "Busca uma Cidade por Id")
-	CidadeModel buscar(Long cidadeId);
+	CidadeModel buscar(@Parameter(description = "ID de uma Cidade", example = "1", required = true) Long cidadeId);
 
 	@Operation(summary = "Cadastra uma Cidade", description = "Cadastro de uma Cidade, necessita de um Estado e um nome válido.")
-	CidadeModel adicionar(CidadeInput cidadeInput);
+	CidadeModel adicionar(@RequestBody(description = "Representação de uma nova Cidade", required = true)  CidadeInput cidadeInput);
 	
 	@Operation(summary = "Atualiza uma Cidade por Id")
-	CidadeModel atualizar(Long cidadeId, CidadeInput cidadeInput);
+	CidadeModel atualizar(@Parameter(description = "ID de uma Cidade", example = "1", required = true) Long cidadeId, 
+			@RequestBody(description = "Representação de uma Cidade com dados atualizados.", required = true)  CidadeInput cidadeInput);
 
 	@Operation(summary = "Exclui uma Cidade por Id")
-	ResponseEntity<Void> remover(Long cidadeId);
+	ResponseEntity<Void> remover(@Parameter(description = "ID de uma Cidade", example = "1", required = true) Long cidadeId);
 	
 }
